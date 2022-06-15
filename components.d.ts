6bffed76cbc6d25e0c8f1cc2e5bc12628e24df15
Soya <3100034521@qq.com>
2022-06-15 14:05:39 +0800

    remove yarn.lock

--- components.d.ts
+++ components.d.ts
@@ -5,10 +5,16 @@ import '@vue/runtime-core'
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
+    AButton: typeof import('@arco-design/web-vue')['Button']
     AEmpty: typeof import('@arco-design/web-vue')['Empty']
+    AFormItem: typeof import('@arco-design/web-vue')['FormItem']
     AImage: typeof import('@arco-design/web-vue')['Image']
+    AInput: typeof import('@arco-design/web-vue')['Input']
     AMenu: typeof import('@arco-design/web-vue')['Menu']
     AMenuItem: typeof import('@arco-design/web-vue')['MenuItem']
+    ASpace: typeof import('@arco-design/web-vue')['Space']
+    ATable: typeof import('@arco-design/web-vue')['Table']
+    ATag: typeof import('@arco-design/web-vue')['Tag']
     AUpload: typeof import('@arco-design/web-vue')['Upload']
     Footer: typeof import('./src/components/Footer.vue')['default']
     Image: typeof import('./src/components/Image.vue')['default']
